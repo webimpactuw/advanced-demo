4f01e3a5f9884420fb9c3910c0ca2320a61f1aa0
blloop <42186044+blloop@users.noreply.github.com>
2024-10-29 19:52:21 -0700

    add person + dog cards via props/fetch

--- app/page.js
+++ app/page.js
@@ -1,65 +1,57 @@
-export default function Page() {
+import Container from "./components/container";
+
+export default function Home() {
+  const person = {
+    name: "John Doe",
+    age: 30,
+    hobbies: ["reading", "hiking", "coding"],
+    address: {
+      street: "123 Main St",
+      city: "New York",
+      state: "NY",
+      zip: "10001",
+    },
+    contact: {
+      email: "john@email.com",
+      phone: "123-456-7890",
+    },
+    isMarried: true,
+  };
+
   return (
-    <>
-      <div className="bg-greenTint h-24 w-48 mx-auto mt-[10vw]" />
-      <p className="m-[10vw] text-white">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et orci
-        massa. Suspendisse pharetra a lorem eu pulvinar. Curabitur in consequat
-        risus, vel gravida est. Morbi rhoncus lectus in volutpat vestibulum.
-        Phasellus id blandit lectus, sit amet tempus turpis. Pellentesque
-        lacinia egestas aliquam. Fusce luctus faucibus vulputate. Integer nec
-        velit pulvinar, fringilla tortor a, gravida ligula. Cras pretium, nunc
-        rhoncus egestas rutrum, magna nunc rhoncus risus, eget auctor tellus leo
-        ac mauris. Curabitur ultricies porta tempus. Praesent neque dolor,
-        condimentum nec sollicitudin et, lacinia in turpis. Maecenas
-        pellentesque lectus mollis, dictum ligula nec, pretium urna. Aenean
-        sollicitudin, odio vel tristique lacinia, ante mauris ornare nisi,
-        laoreet ullamcorper massa enim fermentum lectus. Etiam malesuada magna
-        risus, at mollis sapien vulputate ut. Maecenas mattis volutpat nunc.
-        Vestibulum at nibh eu tellus sodales ultricies. Curabitur a consequat
-        mi. Duis nulla enim, cursus in porta porttitor, fermentum eu leo.
-        Praesent sed nisi egestas, bibendum massa et, porttitor enim. Nam
-        vestibulum eu nibh sit amet gravida. Maecenas non felis elementum,
-        volutpat risus nec, dictum lacus. Morbi dui ex, vestibulum id massa sit
-        amet, tincidunt posuere lectus. Mauris in tortor dapibus, sollicitudin
-        tortor eget, aliquam velit. Proin sed lacus quis nunc finibus
-        scelerisque. Vivamus elementum risus nibh, eget vestibulum sapien
-        placerat at. Aliquam et blandit dui. Etiam vel ligula quis velit
-        facilisis condimentum ut in orci. Pellentesque faucibus fermentum nulla,
-        nec aliquam elit vestibulum ac. Proin ultrices lorem lectus, ultrices
-        semper velit rutrum ac. Maecenas egestas dui ipsum, quis porttitor lorem
-        egestas id. Praesent pellentesque, dui in rutrum vehicula, mi libero
-        ullamcorper eros, vitae aliquet dolor nulla vulputate arcu. Aliquam
-        iaculis tincidunt odio scelerisque tempor. Etiam non nibh tempus, semper
-        ex at, porta nisl. Vestibulum rhoncus erat ullamcorper metus finibus
-        varius. Vestibulum volutpat eleifend lorem, non facilisis erat finibus
-        sit amet. Donec ultrices sollicitudin ex. Nullam euismod quam nec ipsum
-        tempus sodales. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Donec vitae lectus vel augue
-        bibendum tincidunt. Aenean id auctor dui. Aenean suscipit accumsan
-        dapibus. Ut feugiat erat urna, vitae ultrices velit egestas in. Sed
-        varius aliquet libero nec condimentum. Nam faucibus tempor massa ut
-        condimentum. Sed tempor quis orci ut ornare. Sed mattis, velit vitae
-        vulputate pellentesque, ante ante bibendum massa, ut lobortis nisl metus
-        et mauris. Praesent interdum tortor sed tincidunt sodales. Curabitur
-        pretium efficitur lacus at feugiat. Donec ornare facilisis lobortis.
-        Suspendisse volutpat et justo volutpat facilisis. Duis malesuada eget
-        eros ut aliquet. Sed efficitur quis nibh id viverra. Nunc consectetur,
-        nunc sed sagittis vestibulum, lacus augue faucibus sapien, quis
-        vulputate arcu tortor sed arcu. In sapien enim, semper at sollicitudin
-        et, porta quis sapien. Integer cursus eleifend massa volutpat posuere.
-        Vivamus sit amet ex dui. Integer quis consectetur nibh. In fermentum
-        lectus eu euismod hendrerit. Nulla pulvinar faucibus enim, ac interdum
-        ligula condimentum non. Curabitur faucibus mauris quis ipsum finibus, a
-        commodo velit porttitor. Proin rhoncus neque urna, sit amet sodales
-        dolor maximus in. Integer elementum vestibulum urna ac ultricies.
-        Aliquam erat volutpat. Praesent dictum finibus nunc, id luctus dolor.
-        Aenean facilisis finibus nulla, id luctus ipsum. Integer luctus nisi
-        commodo nulla mattis, sed mattis dolor feugiat. Sed neque felis, varius
-        vel mauris vitae, suscipit accumsan nulla. Quisque varius sit amet
-        sapien dignissim placerat. Fusce eu dui luctus, auctor dui et, malesuada
-        urna. Phasellus scelerisque et augue et interdum.
-      </p>
-    </>
+    <Container>
+      <Person person={person} />
+    </Container>
+  );
+}
+
+function Person({ person }) {
+  return (
+    <div className="max-w-sm border border-primary-200 rounded-xl px-4 pb-4 pt-6 bg-gradient-to-bl from-secondary-100 to-white via-white md:hover:shadow-xl md:hover:scale-105 transition-all space-y-4">
+      <div className="space-y-2">
+        <h3 className="font-medium text-xl">{person.name}</h3>
+        <p className="flex items-center justify-start space-x-2 text-xs font-medium uppercase text-primary-600">
+          {person.age} years old & ???
+        </p>
+        <p className="text-sm text-primary-500 underline">
+          {person.street} {person.city} {person.state} {person.zip}
+        </p>
+      </div>
+      <div className="space-y-1">
+        <h4 className="font-medium">Hobbies</h4>
+        <ul className="flex flex-wrap gap-2 text-sm text-secondary-800">
+          <li className="bg-secondary-100 px-2 py-0.5 rounded-full">Hobby 1</li>
+          <li className="bg-secondary-100 px-2 py-0.5 rounded-full">Hobby 2</li>
+          <li className="bg-secondary-100 px-2 py-0.5 rounded-full">Hobby 3</li>
+        </ul>
+      </div>
+      <div className="space-y-1">
+        <h4 className="font-medium">Contact</h4>
+        <ul className="font-mono text-sm">
+          <li>{person.contact.email}</li>
+          <li>{person.contact.phone}</li>
+        </ul>
+      </div>
+    </div>
   );
 }
