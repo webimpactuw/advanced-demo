a46b5c4a8323e5a4bade71bb9f1d2bcc2ea41d2c
blloop <42186044+blloop@users.noreply.github.com>
2024-10-29 20:24:00 -0700

    fetch people from sample api and render

--- app/page.js
+++ app/page.js
@@ -1,57 +1,56 @@
 import Container from "./components/container";
 
-export default function Home() {
-  const person = {
-    name: "John Doe",
-    age: 30,
-    hobbies: ["reading", "hiking", "coding"],
-    address: {
-      street: "123 Main St",
-      city: "New York",
-      state: "NY",
-      zip: "10001",
-    },
-    contact: {
-      email: "john@email.com",
-      phone: "123-456-7890",
-    },
-    isMarried: true,
-  };
+export default async function Home() {
+  const people = await getPeople()
 
   return (
     <Container>
-      <Person person={person} />
+      <div className="flex flex-wrap gap-6">
+        {people.map(p => 
+          <Person key={p.name} person={p} />
+        )}
+      </div>
     </Container>
   );
 }
 
 function Person({ person }) {
   return (
-    <div className="max-w-sm border border-primary-200 rounded-xl px-4 pb-4 pt-6 bg-gradient-to-bl from-secondary-100 to-white via-white md:hover:shadow-xl md:hover:scale-105 transition-all space-y-4">
+    <div className="max-w-sm w-full border border-primary-200 rounded-xl px-4 pb-4 pt-6 bg-gradient-to-bl from-zinc-800 to-zinc-500 md:hover:shadow-xl md:hover:scale-105 transition-all cursor-pointer space-y-2">
       <div className="space-y-2">
         <h3 className="font-medium text-xl">{person.name}</h3>
-        <p className="flex items-center justify-start space-x-2 text-xs font-medium uppercase text-primary-600">
-          {person.age} years old & ???
-        </p>
-        <p className="text-sm text-primary-500 underline">
-          {person.street} {person.city} {person.state} {person.zip}
-        </p>
-      </div>
-      <div className="space-y-1">
-        <h4 className="font-medium">Hobbies</h4>
-        <ul className="flex flex-wrap gap-2 text-sm text-secondary-800">
-          <li className="bg-secondary-100 px-2 py-0.5 rounded-full">Hobby 1</li>
-          <li className="bg-secondary-100 px-2 py-0.5 rounded-full">Hobby 2</li>
-          <li className="bg-secondary-100 px-2 py-0.5 rounded-full">Hobby 3</li>
-        </ul>
+        <div className="flex items-center justify-start space-x-2 text-xs font-medium uppercase text-primary-600">
+          Username: 
+          <p className="bg-zinc-900 ml-2 p-1 rounded-md">
+            {person.username}
+          </p>
+        </div>
       </div>
       <div className="space-y-1">
         <h4 className="font-medium">Contact</h4>
         <ul className="font-mono text-sm">
-          <li>{person.contact.email}</li>
-          <li>{person.contact.phone}</li>
+          <li>{person.email}</li>
+          <li>{person.phone}</li>
         </ul>
       </div>
+      <hr />
+      <div className="flex items-center justify-start space-x-2 text-xs font-medium uppercase text-primary-600">
+        Company: 
+        <p className="bg-slate-700 ml-2 p-1 rounded-md">
+          {person.company.name}
+        </p>
+      </div>
+      <p className="bg-secondary-100 text-sm rounded-full">{person.company.catchPhrase}</p>
     </div>
   );
+}
+
+async function getPeople() {
+  const people = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!people.ok) {
+    return new Error("Could not fetch data!");
+  }
+
+  return people.json();
 }
